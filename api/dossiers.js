19562bccff390381926932663be92be3d0aaa8ba
Thành Đạt <thanhledatomon@gmail.com>
2026-01-25 14:34:29 +0700

    Update dossiers.js

--- api/dossiers.js
+++ api/dossiers.js
@@ -1,7 +1,7 @@
 import { readSession } from "./_auth.js";
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
+  if (req.method !== "GET") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
 
