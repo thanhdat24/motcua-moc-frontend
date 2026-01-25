7977aa26652c951bf4ae57c4d07558b693d291e5
Thành Đạt <thanhledatomon@gmail.com>
2026-01-25 15:30:18 +0700

    Update dossiers.js

--- api/dossiers.js
+++ api/dossiers.js
@@ -30,7 +30,11 @@ export default async function handler(req, res) {
   };
 
   // ✅ nhận token từ FE
-  const { tokenBXD, tokenBYT } = req.body || {};
+  // const { tokenBXD, tokenBYT } = req.body || {};
+
+  const tokenBXD = normalizeBearer(localStorage.getItem("TOKEN_BXD") || "");
+  const tokenBYT = normalizeBearer(localStorage.getItem("TOKEN_BYT") || "");
+
   const bxd = normalizeBearer(tokenBXD);
   const byt = normalizeBearer(tokenBYT);
 
